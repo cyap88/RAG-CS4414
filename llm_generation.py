c7069f1a40c705b917c47afbc441ff0dc43e0441
cyap88 <camille.yap2008@gmail.com>
2025-12-27 23:13:42 -0500

    Remove commented-out code from llm_generation.py

--- llm_generation.py
+++ llm_generation.py
@@ -19,16 +19,3 @@ def llm_generate(llm, I, query, id_to_doc):
 
   generation = (end - start)*1000
   return output["choices"][0]["text"], document_retrieval, prompt_augmentation, generation
-# def llm_generate(llm, I, query, id_to_doc):
-#   prompt = query + "Top Documents:" 
-#   for i in range(len(I[0])):
-#     id = I[0][i]
-#     prompt += id_to_doc[id]['text']
-#     # print(f"Doc {id}: {id_to_doc[id]['text']}")
-#   # end = time.perf_counter()
-#   # elapse = (end - start)*1000
-
-#   # print("Retrieving Documents Done")
-#   # start_generation = time.perf_counter()
-#   output = llm(prompt=prompt, max_tokens=256, stop=["\n"],echo=False)
-#   return output["choices"][0]["text"]
